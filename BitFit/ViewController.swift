2be9ef4e79015a4610720e3e3d11704e68514b09
Michael Dales <mdales@mac.com>
2019-05-21 07:43:25 +0100

    Tidy up UI workflow a little

--- BitFit/ViewController.swift
+++ BitFit/ViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import HealthKit
 import CoreLocation
+import AVKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var toggleButton: UIButton!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     
     let syncQ = DispatchQueue(label: "workout")
     
+    let synthesizer = AVSpeechSynthesizer()
     let healthStore = HKHealthStore()
+    
     let locationManager = CLLocationManager()
     var workoutBuilder: HKWorkoutBuilder?
     var routeBuilder: HKWorkoutRouteBuilder?
@@ -57,10 +62,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func toggleWorkout(_ sender: Any) {
+        
         if workoutBuilder == nil {
-            startWorkout()
+            let alert = UIAlertController(title: "Start workout",
+                                          message: "Are you sure you wish to start a workout?",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                self.startWorkout()
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
         } else {
-            stopWorkout()
+            let alert = UIAlertController(title: "Stop workout",
+                                          message: "Are you sure you wish to stop the workout?",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                self.stopWorkout()
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
@@ -192,6 +213,25 @@ extension ViewController: CLLocationManagerDelegate {
                 distance += location.distance(from: last)
             }
             lastLocation = location
+        }
+        
+        if Int(distance) % 1610 == 0 {
+            
+            let miles = distance / 1609.34
+            let start = self.workoutBuilder!.startDate!
+            let duration = Date().timeIntervalSince(start)
+            let minutes = Int(duration / 60.0)
+            let seconds = Int(duration) - (minutes * 60)
+            
+            DispatchQueue.main.async {
+                self.distanceLabel.text = String(format: "%.1f miles", miles)
+                self.durationLabel.text = String(format: "%d minutes and %d seconds", minutes, seconds)
+                
+                let part1 = AVSpeechUtterance(string: self.distanceLabel.text!)
+                self.synthesizer.speak(part1)
+                let part2 = AVSpeechUtterance(string: self.durationLabel.text!)
+                self.synthesizer.speak(part2)
+            }
         }
         
         builder.insertRouteData(locations) { (success, error) in
